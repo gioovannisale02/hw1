a29b868a1d77cad3728160b08fb6fbcd19d920cd
Giovanni🥤 <giovannisale.11@gmail.com>
2025-06-12 01:08:24 +0200

    logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,10 @@
 <?php
 session_start();
 
-// Elimina tutte le variabili di sessione
 $_SESSION = [];
 
-// Distrugge la sessione
 session_destroy();
 
-// Reindirizza alla pagina di login
 header("Location: login.php");
 exit;
 ?>
